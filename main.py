89aeb9d779eeef78f4580b4ab0717c1b65a4dd53
romusters <romusters@gmail.com>
2019-02-13 13:35:57 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from sklearn.neighbors import NearestNeighbors
+from random import sample
+from numpy.random import uniform
+import numpy as np
+from math import isnan
+ 
+def hopkins(X):
+    d = X.shape[1]
+    #d = len(vars) # columns
+    n = len(X) # rows
+    m = int(0.1 * n) # heuristic from article [1]
+    nbrs = NearestNeighbors(n_neighbors=1).fit(X.values)
+ 
+    rand_X = sample(range(0, n, 1), m)
+ 
+    ujd = []
+    wjd = []
+    for j in range(0, m):
+        u_dist, _ = nbrs.kneighbors(uniform(np.amin(X,axis=0),np.amax(X,axis=0),d).reshape(1, -1), 2, return_distance=True)
+        ujd.append(u_dist[0][1])
+        w_dist, _ = nbrs.kneighbors(X.iloc[rand_X[j]].values.reshape(1, -1), 2, return_distance=True)
+        wjd.append(w_dist[0][1])
+ 
+    H = sum(ujd) / (sum(ujd) + sum(wjd))
+    if isnan(H):
+        print ujd, wjd
+        H = 0
+ 
+    return H
